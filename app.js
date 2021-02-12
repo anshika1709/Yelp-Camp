80cda520426d14d37c0a00f0a4088642bcc17327
anshika1709 <awasthi.anshika1709@gmail.com>
2021-02-12 16:48:53 +0530

    Added basic Create and View operation for camps

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const Campground = require('./models/campground');
 const mongoose = require('mongoose');
+const campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
@@ -12,20 +13,38 @@ db.once('open', function() {
     console.log("connected");
 });
 
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-// respond with "hello world" when a GET request is made to the homepage
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/campgrounds', async(req, res) => {
-    const newCamp = new Campground({
-        title: "ABCamp",
+    const allCampgrounds = await Campground.find({});
+    res.render("campgrounds/index", { allCampgrounds });
+})
 
-    });
-    await newCamp.save();
-    res.send(newCamp);
+app.get('/campgrounds/new', async(req, res) => {
+    res.render("campgrounds/new");
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+
+    res.redirect(`campgrounds/${newCampground._id}`);
+
+
+})
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campgroundById = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campgroundById });
 })
 
 app.listen(3000, () => {
