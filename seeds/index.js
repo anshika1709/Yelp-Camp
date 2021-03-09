7487594b9a47d9eb1a00fb63b31544b849705e4d
anshika1709 <awasthi.anshika1709@gmail.com>
2021-03-09 13:09:41 +0530

    Add multiple ima0ge upload feature

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,13 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/anshika/image/upload/v1615115277/Camping%20site/ramly6mrt9cfhy3wulzv.jpg',
